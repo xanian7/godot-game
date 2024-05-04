bd5b3583b5c7c1275632a30e83d0bde4a042ed65
xanian7 <xanian407@gmail.com>
2024-05-04 17:38:56 -0400

    movement fix for animation thingy later on

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Reflection.Metadata;
 
 public partial class Player : RigidBody2D
 {
@@ -18,27 +19,10 @@ public partial class Player : RigidBody2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		var velocity = Vector2.Zero; // The player's movement vector.
-	
-		if (Input.IsActionPressed("move_right"))
-		{
-			velocity.X += 1;
-		}
-
-		if (Input.IsActionPressed("move_left"))
-		{
-			velocity.X -= 1;
-		}
-
-		if (Input.IsActionPressed("move_down"))
-		{
-			velocity.Y += 1;
-		}
-
-		if (Input.IsActionPressed("move_up"))
-		{
-			velocity.Y -= 1;
-		}
+		var velocity = Vector2.Zero;
+		
+		var action = GetPlayerAction();
+		velocity = HandleMovement(action);
 
 		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 
@@ -54,4 +38,63 @@ public partial class Player : RigidBody2D
 		
 		Position += velocity * (float)delta;
 	}
+
+	public static PlayerAction GetPlayerAction()
+	{
+		if (Input.IsActionPressed("move_right") && Input.IsActionPressed("move_up")) return PlayerAction.move_right_up;
+		if (Input.IsActionPressed("move_right") && Input.IsActionPressed("move_down")) return PlayerAction.move_right_down;
+		if (Input.IsActionPressed("move_left") && Input.IsActionPressed("move_up")) return PlayerAction.move_left_up;
+		if (Input.IsActionPressed("move_left") && Input.IsActionPressed("move_down")) return PlayerAction.move_left_down;
+		if (Input.IsActionPressed("move_right")) return PlayerAction.move_right;
+		if (Input.IsActionPressed("move_left")) return PlayerAction.move_left;
+		if (Input.IsActionPressed("move_down")) return PlayerAction.move_down;
+		if (Input.IsActionPressed("move_up")) return PlayerAction.move_up;
+
+		//if no input is pressed
+		return PlayerAction.idle;	
+	}
+
+    public static Vector2 HandleMovement(PlayerAction action)
+	{
+		var velocity = Vector2.Zero;
+        _ = action switch
+        {
+			PlayerAction.move_right_up => velocity.X += 1,
+			PlayerAction.move_right_down => velocity.X += 1,
+			PlayerAction.move_left_down => velocity.X -= 1,
+			PlayerAction.move_left_up => velocity.X -= 1,
+            PlayerAction.move_right => velocity.X += 1,
+            PlayerAction.move_left => velocity.X -= 1,
+            PlayerAction.move_down => velocity.Y += 1,
+            PlayerAction.move_up => velocity.Y -= 1,
+			PlayerAction.idle => 0,
+            _ => 0
+        };
+
+		//handing movement along Y axis for diagonal action
+		_ = action switch
+        {
+            PlayerAction.move_right_up => velocity.Y -= 1,
+			PlayerAction.move_left_up => velocity.Y -= 1,
+			PlayerAction.move_right_down => velocity.Y += 1,
+			PlayerAction.move_left_down => velocity.Y += 1,
+            _ => 0
+        };
+
+        return velocity;
+	}
+
+	public enum PlayerAction 
+	{
+		move_right,
+		move_left,
+		move_down,
+		move_up,
+		move_right_up,
+		move_right_down,
+		move_left_up, 
+		move_left_down,
+		idle
+	}
+
 }
